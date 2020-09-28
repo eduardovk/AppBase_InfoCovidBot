fc9af86bb7fc28736f8924045b9d973b7c7369fd
eduardovk <eduardo_vk@hotmail.com>
2020-09-28 00:55:07 -0300

    Adicionada rota e metodo get para entidade Status

--- app.js
+++ app.js
@@ -2,9 +2,24 @@ const express = require('express');
 
 const app = express();
 
+const rotaStatus = require('./routes/status'); //importa arquivo status.js
+
+app.use('/status', rotaStatus); //define a rota status
+
+//quando não encontra a rota, devolve erro
 app.use((req, res, next) => {
-    res.status(200).send({
-        mensagem: 'OK, funcionando!'
+    const erro = new Error("Não encontrado"); //define message do erro
+    erro.status = 404; //define status do erro
+    next(erro); //passa a variavel 'erro' para o proximo middleware (abaixo)
+});
+
+//captura e exibe msgs de erro
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    return res.send({
+        erro: {
+            mensagem: error.message
+        }
     });
 });
 
---
+++ routes/status.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).send({
+        mensagem: 'Em desenvolvimento'
+    });
+});
+
+module.exports = router;
