c8989b36af2d69ad05cb6478d56c54af1f7c2232
eduardovk <eduardo_vk@hotmail.com>
2020-09-28 01:46:23 -0300

    Definidos headers e permissoes de CORS

--- app.js
+++ app.js
@@ -4,6 +4,23 @@ const app = express();
 
 const rotaStatus = require('./routes/status'); //importa arquivo status.js
 
+//definicao de cabecalhos
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); //header para permitir Cross-Origin
+    res.header(
+        'Access-Control-Allow-Header',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+
+    //se o metodo do request for OPTIONS, retorna cabecalho com lista de metodos aceitos
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).send({}); //envia resposta vazia
+    }
+
+    next(); //chama o proximo middleware
+});
+
 app.use('/status', rotaStatus); //define a rota status
 
 //quando não encontra a rota, devolve erro
