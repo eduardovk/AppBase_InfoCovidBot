93b8b3c8e21c77f567823ce713bf4156089849fd
eduardovk <eduardo_vk@hotmail.com>
2020-10-24 19:57:30 -0300

    Removido require de app.js em server.js

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 const http = require('http'); //importa biblioteca http
-const app = require('./app'); //importa app criado
+const routes = require('./routes'); //importa rotas
 const port = process.env.PORT || 3000  //define porta padrao
 
-
-const server = http.createServer(app); //cria server com o app.js
-
-
+const server = http.createServer(routes); //cria server com o routes.js
 
 server.listen(port); //inicia o server ouvindo a porta padrao
