db03523268a75cbde28ca70c99fa386294de3d29
eduardovk <eduardo_vk@hotmail.com>
2020-10-24 21:31:34 -0300

    App.js adicionado novamente

--- routes.js
+++ routes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const ApiPais = require('./services/pais');
 
 
-
 router.get('/ping', (req, res) => {
     res.send(send(200));
 });
@@ -21,11 +20,11 @@ router.post('/', async (req, res) => {
                 response = send(200, 'ping!');
                 break;
             case 'paisCasos': 
-                response = send(200, api.validateCpf(req.body.params));
+                // response = send(200, api.validateCpf(req.body.params));
                 break;
             case 'paisMortos':
-                data = await api.login(req.body.params);
-                response = data ? send(200, data) : send(400);
+                // data = await api.login(req.body.params);
+                // response = data ? send(200, data) : send(400);
                 break;
             default:
                 log('error', 'Requisição não identificada');
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http'); //importa biblioteca http
-const routes = require('./routes'); //importa rotas
+const app = require('./app'); //importa app criado
 const port = process.env.PORT || 3000  //define porta padrao
 
-const server = http.createServer(routes); //cria server com o routes.js
+const server = http.createServer(app); //cria server com o app.js
 
 server.listen(port); //inicia o server ouvindo a porta padrao
