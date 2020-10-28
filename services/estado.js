0ea903523b2dc539fc21f2374463684b48f19299
eduardovk <eduardo_vk@hotmail.com>
2020-10-28 16:37:59 -0300

    Adicionado nome do estado na resposta da consulta de dados do estado

--- services/estado.js
+++ services/estado.js
@@ -6,6 +6,36 @@ const { DateTime } = require("luxon");
 
 const router = express.Router();
 
+const nomeEstados ={
+     'AC': 'Acre',
+     'AL': 'Alagoas',
+     'AP': 'Amapá',
+     'AM': 'Amazonas',
+     'BA': 'Bahia',
+     'CE': 'Ceará',
+     'DF': 'Distrito Federal',
+     'ES': 'Espírito Santo',
+     'GO': 'Goías',
+     'MA': 'Maranhão',
+     'MT': 'Mato Grosso',
+     'MS': 'Mato Grosso do Sul',
+     'MG': 'Minas Gerais',
+     'PA': 'Pará',
+     'PB': 'Paraíba',
+     'PR': 'Paraná',
+     'PE': 'Pernambuco',
+     'PI': 'Piauí',
+     'RJ': 'Rio de Janeiro',
+     'RN': 'Rio Grande do Norte',
+     'RS': 'Rio Grande do Sul',
+     'RO': 'Rondônia',
+     'RR': 'Roraíma',
+     'SC': 'Santa Catarina',
+     'SP': 'São Paulo',
+     'SE': 'Sergipe',
+     'TO': 'Tocantins'
+};
+
 class APIEstado {
     constructor() { }
 
@@ -35,6 +65,7 @@ class APIEstado {
                                     status: 200,
                                     body: {
                                         uf: estado.uf,
+                                        nome: nomeEstados[estado.uf],
                                         confirmados: formatarNumero(estado.cases),
                                         obitos: formatarNumero(estado.deaths),
                                         atualizado_em: atualizadoEm
