9853a70e931963e8cd4f3775e1c988a6649ff6bf
eduardovk <eduardo_vk@hotmail.com>
2020-11-18 01:24:42 -0300

    Adicionada opcao de converter apenas data no helper dataHora.js

--- helpers/dataHora.js
+++ helpers/dataHora.js
@@ -1,9 +1,12 @@
-module.exports = function formatarDataHora(data) {
+module.exports = function formatarDataHora(data, hora = true) {
     var novaData = new Date(data);
     var dia = novaData.getDate().toString().padStart(2, '0');
     var mes = (novaData.getMonth() + 1).toString().padStart(2, '0');
     var ano = novaData.getFullYear();
-    var hora = novaData.getHours();
-    var min = novaData.getMinutes();
-    return dia + "/" + mes + "/" + ano + "  " + hora + ":" + min;
+    if(hora){
+        var hora = novaData.getHours();
+        var min = novaData.getMinutes();
+        return dia + "/" + mes + "/" + ano + "  " + hora + ":" + min;
+    }
+    return dia + "/" + mes + "/" + ano;
 }
